ec910ba02aa0b16984fc236366484d674dfc855f
massimogiordano <massimogiordano@eduroam-193-157-113-234.uio.no>
2014-08-21 11:08:03 +0200

    es1

--- es1-1/main.cpp
+++ es1-1/main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
+/* comments in C begin like this and end with */
+#include <stdlib.h> /* atof function */
+#include <math.h>   /* sine function */
+#include <stdio.h>  /* printf function */
 
-using namespace std;
-
-int main()
+int main (int argc, char* argv[])
 {
-    cout << "Hello World22!" << endl;
-    return 0;
+  double r, s;        /* declare variables */
+  r = atof(argv[1]);  /* convert the text argv[1] to double */
+  s = sin(r);
+  printf("Hello, World! sin(%g)=%g\n", r, s);
+  return 0;           /* success execution of the program */
 }
-
